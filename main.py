b57c866c0dd465b674fa214e6c72c0787b168eda
shineyagomi <haruto10100432@icloud.com>
2025-05-03 17:40:01 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -66,4 +66,18 @@ def keep_alive():
     t.start()
 
 keep_alive()
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running."
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
 bot.run(TOKEN)
